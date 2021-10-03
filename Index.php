c02dde9bc5d556aa86b3da91a5b343a6fd23fcec
Rajneesh <68689523+Rajneesh-coder@users.noreply.github.com>
2021-10-03 19:40:25 +0530

    Revert "Website project "

--- Index.php
+++ Index.php
@@ -1,15 +1,7 @@
 <?php
 ?>
 <html>
-<head>
-  <meta charset="UTF-8">
-
-<meta name="description" content="website project discription">
-
-<meta name="keywords" content="HTML, CSS, JavaScript">
-<title>Website Project</title>
-<meta name="author" content="name ">
-</head>
+<head></head>
 <body></body>
 
 </html>
